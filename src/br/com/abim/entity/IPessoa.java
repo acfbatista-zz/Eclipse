e1b86d277fe5e30991ca8c5b28fc0f0d02e3e690
Antonio <Antonio@comapi.embarcadero.com>
2010-01-01 02:03:14 -0300

    Adicionada a classe Motorista

---
+++ src/br/com/abim/entity/IPessoa.java
@@ -0,0 +1,15 @@
+package br.com.abim.entity;
+
+public interface IPessoa {
+
+	Long getId();
+
+	void setId(Long id);
+
+	String getNome();
+
+	void setNome(String nome);
+
+	String toString();
+
+}
---
+++ src/br/com/abim/entity/Motorista.java
@@ -0,0 +1,29 @@
+package br.com.abim.entity;
+
+public class Motorista implements IPessoa {
+
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setId(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNome(String nome) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
